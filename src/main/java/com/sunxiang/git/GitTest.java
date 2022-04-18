56b2480ec1c5f4049cff7f26ca44164a97eababe
SunXiang <577278997@qq.com>
2022-04-18 13:14:33 +0800

    second commit

--- src/main/java/com/sunxiang/git/GitTest.java
+++ src/main/java/com/sunxiang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.sunxiang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
