a2a5032877021b7a07f4ddfd27cf2b4f04d76b3d
SunXiang <577278997@qq.com>
2022-04-19 10:30:34 +0800

    master commit1

--- src/main/java/com/sunxiang/git/GitTest.java
+++ src/main/java/com/sunxiang/git/GitTest.java
@@ -3,5 +3,8 @@ package com.sunxiang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello sunxiang");
+        System.out.println("hello master test commit");
     }
 }
