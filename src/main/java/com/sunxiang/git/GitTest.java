9edd247eaca408b5776aa7e474ada6c180b494cc
sunxiangtest1 <103868926+sunxiangtest1@users.noreply.github.com>
2022-04-20 09:32:10 +0800

    444

--- src/main/java/com/sunxiang/git/GitTest.java
+++ src/main/java/com/sunxiang/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello hot-fix commit");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
